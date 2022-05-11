42035b5ef4777357e03e8190d924e7e441849431
unknown <makaryyrakam@gmail.com>
2022-05-11 22:48:20 +0200

    improved looks of message received

--- controllers/mail.js
+++ controllers/mail.js
@@ -6,7 +6,13 @@ dotenv.config();
 const mg = mailgun({ apiKey: process.env.API_KEY, domain: process.env.DOMAIN });
 
 export const sendMail = (req, res) => {
-    mg.messages().send(req.body, (error, body) => {
-        console.log(body);
+    const mailData = {
+        from: req.body.email,
+        to: "kubaw.iluzja@gmail.com",
+        subject: `${req.body.email} wysłał wiadomość ze strony kubawarkocz.pl`,
+        html: `<html><body><h3>Dostałeś wiadomość od ${req.body.email} o treści:</h3><p>${req.body.message}</p></body></html>`
+    };
+    mg.messages().send(mailData, () => {
+        console.log(`Sending message from ${req.body.email}...`);
     });
 };
